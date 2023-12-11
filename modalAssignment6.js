7262bc166877deb903eda081c9f2ecaef716d3e3
Vaibhav Kasana <125895773+VaibhavKasana2511@users.noreply.github.com>
2023-12-11 07:50:19 +0530

    Update modalAssignment6.js

--- modalAssignment6.js
+++ modalAssignment6.js
@@ -8,47 +8,22 @@ import {
 import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/AntDesign';
 
-export default function PracticeModal({ closeModal, OnDataPassed }) {
-  let newUser = {
-    name: '',
-    medCategories: [{ type: '', med: [{ name: '' }] }],
-  };
+export default function PracticeModal({ closeModal,statePass}) {
+  console.log("DATAPASSED",statePass)
   const [userData, setUserData] = useState({
     name: '',
     medCategories: [],
   });
-  const [count, setCount] = useState(0);
+
   const addMedicineType = () => {
     let med_categories = [...userData.medCategories];
     console.log('medCategories', med_categories);
-    med_categories.push({ type: '', med: []});
+    med_categories.push({ type: '', med: [] });
     console.log('ABCDEFGH', userData);
-
-    // setUserData(Object.assign({}, userData, { medCategories: med_categories }));
-    setUserData({ ...userData,  medCategories: med_categories });
-
+    setUserData({ ...userData, medCategories: med_categories });
 
     console.log('ABCDEFGH22222222', userData);
   };
-
-//   const addMedicine = (index) => {
-//     // console.log('index', index);
-//     let medicine = [...userData.medCategories[index].med]
-
-//     medicine.push({ name: '' });
-//     const hehe = userData.medCategories
-//     obj1=Object.assign([],hehe,med=medicine)
-
-//     // console.log(userData,'above',medicine)
-// const data= {...userData,medCategories:[{...userData.medCategories[index],med:medicine}]}
-
-
-// console.log('abc',data,'jsadksahjkß')
-//     setUserData(data)
-//     // setUserData(Object.assign({}, userData, { medCategories: Object.assign([],hehe,med=medicine) }));
-
-//     // console.log('Add Medicine', Object.assign({}, userData, { medCategories: Object.assign([],hehe,med=medicine) }))
-//   }
 
   const addMedicine = (index) => {
     setUserData((prevData) => {
@@ -66,13 +41,47 @@ export default function PracticeModal({ closeModal, OnDataPassed }) {
     });
   };
 
-  const handleHobbyInput = (text, index) => {
-    let all_hobbies = [...userData.title];
-    setUserData(
-      Object.assign({}, userData, {
-        title: Object.assign([], all_hobbies, { [index]: { hobby: text } }),
-      })
-    );
+  const handleMedicineTypeChange = (index, text) => {
+    console.log('TEXT', text);
+    setUserData((prevData) => {
+      return {
+        ...prevData,
+        medCategories: prevData.medCategories.map((category, i) => {
+          if (i === index) {
+            return {
+              ...category,
+              type: text,
+            };
+          }
+          return category;
+        }),
+      };
+    });
+  };
+
+  const handleMedChange = (categoryIndex, medIndex, newName) => {
+    setUserData((prevData) => {
+      return {
+        ...prevData,
+        medCategories: prevData.medCategories.map((category, i) => {
+          if (i === categoryIndex) {
+            return {
+              ...category,
+              med: category.med.map((medicine, j) => {
+                if (j === medIndex) {
+                  return {
+                    ...medicine,
+                    name: newName,
+                  };
+                }
+                return medicine;
+              }),
+            };
+          }
+          return category;
+        }),
+      };
+    });
   };
 
   // const renderMedicine = () => {
@@ -106,54 +115,68 @@ export default function PracticeModal({ closeModal, OnDataPassed }) {
   //   };
 
   const renderInput = () => {
+    console.log('FULL DATA', userData);
     console.log('UNDEFINED', userData.medCategories);
-    return  userData.medCategories.map((medCategories, index) => (
-      <View>
-        <View
-          key={index}
-          style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <TextInput
-            style={styles.inputStyle2}
-            placeholder="Medicine Type"
-            value={medCategories.type}
-            // onChangeText={(text) => handleHobbyInput(text, index)}
-          />
-          {index === userData.medCategories.length - 1 && (
-            <TouchableOpacity
-              onPress={() => addMedicine(index)}
-              style={{
-                borderWidth: 1,
-                marginVertical: 7,
-                justifyContent: 'center',
-              }}>
-              <Text style={{ color: '#001965' }}> Add Medicine </Text>
-            </TouchableOpacity>
-          )}
-        </View>
-        {medCategories.med.map((data, i) => (
-          <View
-            key={index}
-            style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <TextInput
-              style={styles.inputStyle2}
-              placeholder="Medicine Name"
-              value={data.name}
-              // onChangeText={(text) => handleHobbyInput(text, index)}
-            />
-            {index === userData.medCategories.length - 1 && (
-              <TouchableOpacity onPress={() => addMedicine(index)}>
-                <Icon
-                  style={styles.Icon}
-                  name="pluscircle"
-                  size={35}
-                  color="black"
+    return userData.medCategories.map(
+      (medCategories, index) => (
+        console.log('INDEX@', index),
+        (
+          <View>
+            <View
+              key={index}
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <TextInput
+                style={styles.inputStyle2}
+                placeholder="Medicine Type"
+                value={medCategories.type}
+                onChange={(e) =>
+                  handleMedicineTypeChange(index, e.target.value)
+                }
+              />
+              {index === userData.medCategories.length - 1 && (
+                <TouchableOpacity
+                  onPress={() => addMedicine(index)}
+                  style={{
+                    borderWidth: 1,
+                    marginVertical: 7,
+                    justifyContent: 'center',
+                  }}>
+                  <Text style={{ color: '#001965' }}> Add Medicine </Text>
+                </TouchableOpacity>
+              )}
+            </View>
+            {medCategories.med.map((data, i) => (
+              <View
+                key={index}
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'flex-end',
+                  gap: 5,
+                }}>
+                <TextInput
+                  style={styles.inputStyle3}
+                  placeholder="Medicine Name"
+                  value={data.name}
+                  onChange={(e) =>
+                    handleMedChange(index, i, e.target.value)
+                  }
                 />
-              </TouchableOpacity>
-            )}
+                {index === data.length && (
+                  <TouchableOpacity onPress={() => addMedicine(index)}>
+                    <Icon
+                      style={styles.Icon}
+                      name="pluscircle"
+                      size={35}
+                      color="black"
+                    />
+                  </TouchableOpacity>
+                )}
+              </View>
+            ))}
           </View>
-        ))}
-      </View>
-    ));
+        )
+      )
+    );
   };
 
   return (
@@ -170,9 +193,9 @@ export default function PracticeModal({ closeModal, OnDataPassed }) {
         <TextInput
           style={styles.inputStyle}
           placeholder="Name"
-          // onChangeText={(text) =>
-          //   setUserData(Object.assign({}, userData, { id: text }))
-          // }
+          onChangeText={(text) =>
+            setUserData(Object.assign({}, userData, { name: text }))
+          }
         />
       </View>
       <View style={{ flexDirection: 'row', alignItems: 'flex-end' }}>
@@ -189,6 +212,16 @@ export default function PracticeModal({ closeModal, OnDataPassed }) {
         </TouchableOpacity>
       </View>
       {renderInput()}
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => {
+        closeModal();
+        statePass(userData);}}
+      >
+        <Text style={{ color: 'white', fontSize: 20, paddingVertical: 10 }}>
+          ADD PHARMACY ..
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -199,10 +232,11 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   addButton: {
-    borderWidth: 1,
     alignItems: 'center',
-    color: 'blue',
+    color: 'white',
+    backgroundColor: 'blue',
     padding: 2,
+    borderRadius: 10,
   },
   inputStyle: {
     backgroundColor: 'pink',
@@ -221,8 +255,16 @@ const styles = StyleSheet.create({
     marginVertical: 7,
   },
 
+  inputStyle3: {
+    backgroundColor: '#4392F1',
+    borderRadius: 10,
+    width: '75%',
+    paddingLeft: 15,
+    paddingVertical: 10,
+    marginVertical: 7,
+  },
+
   Icon: {
     marginVertical: 7,
   },
 });
-
